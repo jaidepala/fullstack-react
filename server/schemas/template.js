bb8d90c4a408a1114ae26784a453d98455ae7723
jay depala <jaidepala@github.com>
2019-04-14 20:24:57 +0530

    create form

---
+++ server/schemas/template.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const TemplateSchema = new Schema(
+  	{
+	    type: String,
+    	label?: String,
+    	placeholder?: String,
+    	helperText?: String,
+    	options?: Array
+  	},
+  	{ 
+  		timestamps: true 
+  	}
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Template", TemplateSchema);
